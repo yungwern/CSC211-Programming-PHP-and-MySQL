0dfcc686e19a1c9ec5141db39f07d5161594dae8
yungwern <83782648+yungwern@users.noreply.github.com>
2026-02-01 15:38:47 -0700

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Variables</title>
+</head>
+<body>
+<?php // Script 2.3 - variables.php
+
+// Personal info:
+$firstname = "Eli";
+$lastname = "Werner";
+$address = "1615 Joshua Tree St.";
+$phone = "530-920-1951";
+
+// Print the personal info:
+print "<p>My name is $firstname $lastname <br>
+I live at $address and my phone number is $phone</p>";
+
+?>
+</body>
+</html>
