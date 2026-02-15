4446cc689d39a9e9206c39d0b10d2911cd04f1f2
yungwern <83782648+yungwern@users.noreply.github.com>
2026-02-15 14:50:10 -0700

    Add files via upload

---
+++ handle_reg7.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Registration</title>
+	<style type="text/css" media="screen">
+		.error { color: red; font-weight: bold; }
+		.success { color: green; font-weight: bold; }
+	</style>
+</head>
+<body>
+<h1>Registration Results</h1>
+<?php // Script 6.2 - handle_reg.php
+/* This script receives seven values from register.html:
+email, password, confirm, year, terms, color, submit */
+
+// Address error management, if you want.
+
+// Flag variable to track success:
+$okay = true;
+
+// Validate email:
+if (empty($_POST['email'])) {
+	print '<p class="error">Please enter your email address.</p>';
+	$okay = false;
+}
+
+// Validate password:
+if (empty($_POST['password'])) {
+	print '<p class="error">Please enter your password.</p>';
+	$okay = false;
+}
+
+// Validate confirm password and check if it matches:
+if (empty($_POST['confirm'])) {
+	print '<p class="error">Please confirm your password.</p>';
+	$okay = false;
+} elseif ($_POST['password'] != $_POST['confirm']) {
+	print '<p class="error">Your passwords do not match.</p>';
+	$okay = false;
+}
+
+// Validate month:
+if (empty($_POST['month'] == 'Month')) {
+	print'<p class="error">Please enter a valid month.</p>';
+	$okay = false;
+}
+	
+// Validate day:
+if (empty($_POST['day'] == 'Day')) {
+	print'<p class="error">Please enter a valid day.</p>';
+	$okay = false;	
+}
+	
+// Validate year and check if it's numeric:
+if (empty($_POST['year']) || $_POST['year'] == 'YYYY') {
+	print '<p class="error">Please enter the year you were born.</p>';
+	$okay = false;
+// Added check to ensure that string length of numeric value is equal to 4.
+} elseif (!is_numeric($_POST['year']) || strlen($_POST['year']) != 4) {
+	print '<p class="error">Please enter a valid numeric year and ensure that its 4 digits.</p>';
+	$okay = false;	
+// I already had this part of the assignment included from previous versions. I did it a bit differently than the book
+// but we come to the same conclusion.
+} elseif ($_POST['year'] >= 2026) {
+	print '<p class="error">Either you entered your birth year wrong or you come from the future!</p>';
+	$okay = false;
+} else {
+	$age = 2026 - $_POST['year']; // Calculate age this year.
+}
+
+// Validate color:
+if (empty($_POST['color'])) {
+	print '<p class="error">Please select your favorite color.</p>';
+	$okay = false;
+}
+
+// Validate terms:
+if (!isset($_POST['terms'])) {
+	print '<p class="error">You must agree to the terms.</p>';
+	$okay = false;
+}
+// If there were no errors, print a success message:
+if ($okay) {
+	print "<p class='success'>You have been successfullly registered (but not really).</p>";
+	print "<p class='success'>You will turn $age this year.</p>";
+}
+?>
+</body>
+</html>
