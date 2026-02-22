c1325a24220cbc497c96822954fa5f54ba171887
yungwern <83782648+yungwern@users.noreply.github.com>
2026-02-22 12:54:15 -0700

    Add files via upload

--- books.php
+++ books.php
@@ -1,45 +1,20 @@
-<!doctype html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Larry Ullman's Boooks and Chapters</title>
-</head>
-<body>
-<h1>Some of Larry Ullman's Books</h1>
-<?php // Script 7.4 - books.php
-/* This script creates and prints out a multidimensional array.
-Address error management, if you want. */
+<?php // Script 8.5 - books.php
+/* This page lists J.D. Salinger's bibliography. */
 
-// Create the first array:
-$phpvqs = [1 => 'Getting Started with PHP', 'Variables', 'HTML Forms and PHP', 'Using Numbers'];
+// Set the page title and include the header file:
+define('TITLE', 'Books by J.D. Slaginer');
+include('templates/header.html');
 
-// Create the second array:
-$phpadv = [1 => 'Advanced PHP Techniques', 'Developing Web Applications', 'Advanced Database Concepts', 'Basic Object-Oriented Programming'];
-
-// Create the third array:
-$phpmysql = [1 => 'Introduction to PHP', 'Programming with PHP', 'Creating Dynamic Web Sites', 'Introduction to MySQL'];
-
-// Create the multidimensional array:
-$books = [
-	'PHP VQS' => $phpvqs,
-	'PHP Advanced VQP' => $phpadv,
-	'PHP and MySQL VQP' => $phpmysql
-];
-
-// Print out some values:
-print "<p>The third chapter of my first book is <i>{$books['PHP VQS'][3]}</i>.</p>";
-print "<p>The first chapter of my second book is <i>{$books['PHP Advanced VQP'][1]}</i>.</p>";
-print "<p>The fourth chapter of my fourth book is <i>{$books['PHP and MySQL VQP'][4]}</i>.</p>";
-
-// See what happens with foreach:
-foreach ($books as $title => $chapters){
-	print "<p>$title";
-	foreach ($chapters as $number => $chapter){
-		print "<br/>Chapter $number is $chapter";
-	}
-	print '</p>';
-}
-
+// Leave the PHP section to display lots of HTML:
 ?>
-</body>
-</html>
+
+<h2>J.D. Salinger's Books</h2>
+<ul>
+	<li>The Catcher in the Rye</li>
+	<li>Nine Stories</li>
+	<li>Franny and Zooey</li>
+	<li>Raise High the Roof Beam, Carpenters and Seymour: An Introduction</li>
+</ul>
+
+<?php include('templates/footer.html');
+?>
